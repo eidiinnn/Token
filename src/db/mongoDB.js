b996fc4f60b095fbae2e4f0ab4ece874fa8e1ee9
Eduardo da Silva <67817853+eidiinnn@users.noreply.github.com>
2021-11-29 12:28:50 +0000

    getToken function and new error catch system

--- src/db/mongoDB.js
+++ src/db/mongoDB.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const mongoDB = {
   uri: "mongodb://172.17.0.1:27017/tokens",
   ModelToken: mongoose.model("token", {
-    token: Object,
+    normalToken: Object,
     refreshToken: Object,
     ip: String,
   }),
@@ -17,17 +17,29 @@ const mongoDB = {
     });
   },
 
-  registerNewToken(token, refreshToken, ip) {
+  registerNewToken(normalToken, refreshToken, ip) {
     toDB = this.ModelToken({
-      token: token,
+      normalToken: normalToken,
       refreshToken: refreshToken,
       ip: ip,
     });
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
       toDB.save((err) => {
         if (!err) return resolve();
-        else throw err;
+        reject([500, {'error': "can't register a new token in the database"}]);
       });
+    });
+  },
+
+  getToken(token) {
+    return new Promise((resolve, reject) => {
+      this.ModelToken.findOne(
+        { normalOrRefresh: Object, token: token },
+        (err, obj) => {
+          if (!err) return resolve(obj);
+          reject([500, {'error': "can't get a token from the database"}]);
+        }
+      );
     });
   },
 };
